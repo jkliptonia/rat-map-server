ed6d2816180b1a42baf4624a2cc504485c16a7eb
Jennifer Lipton <jlipton@marketo.com>
2019-11-15 17:10:05 -0800

    Seperated out GraphQL files

--- src/index.js
+++ src/index.js
@@ -1,70 +1,20 @@
 import express from 'express';
-import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServer } from 'apollo-server-express';
 import cors from 'cors';
+
+import schema from './schema';
+import resolvers from './resolvers';
+import models from './models';
 
 const app = express();
 app.use(cors());
 
-const schema = gql`
-  type Query {
-    users: [User!]
-    me: User
-    user(id: ID!): User
-  }
-  type User {
-    id: ID!
-    username: String!
-  }
-  type Mutation {
-    createUser(name: String!): User!
-    deleteUser(id: ID!): Boolean!
-  }
-`;
-
-let users = {
-  1: {
-    id: '1',
-    username: 'Robin Wieruch'
-  },
-  2: {
-    id: '2',
-    username: 'Dave Davids'
-  }
-};
-const me = users[1];
-const resolvers = {
-  Query: {
-    users: () => Object.values(users),
-    user: (parent, { id }) => users[id],
-    me: () => me
-  },
-  User: {
-    username: user => user.username
-  },
-  Mutation: {
-    createUser: (parent, { name }) => {
-      const user = {
-        id: Object.keys(users).length + 1,
-        username: name
-      };
-
-      users[user.id] = user;
-      return user;
-    },
-    deleteUser: (parent, { id }) => {
-      const { [id]: user, ...otherUsers } = users;
-      if (!user) {
-        return false;
-      }
-      users = otherUsers;
-      return true;
-    }
-  }
-};
-
 const server = new ApolloServer({
   typeDefs: schema,
-  resolvers
+  resolvers,
+  context: {
+    ratGenome: models.genome
+  }
 });
 server.applyMiddleware({ app, path: '/graphql' });
 app.listen({ port: 8000 }, () => {
